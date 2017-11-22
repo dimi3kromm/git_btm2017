24a34502654ae4ff837d8afa504208beaf6fcf7e
dimi3kromm <dimitri.kromm@embl.de>
2017-11-22 15:06:07 +0000

    first_steps

---
+++ lovely_solution/functionalities.py
@@ -0,0 +1,12 @@
+import file_manager as fM
+
+def say_hi():
+	print("Hello there! It's lovely to welcome such a loyal customer back! How can I help you?")
+def new_client():
+    pass
+    
+def new_transaction():
+    pass
+    
+def look_credit():
+    pass
