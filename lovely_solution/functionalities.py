605c58751f555c34e57f5cd2fcc75e2d4ab81137
dimi3kromm <dimitri.kromm@embl.de>
2017-11-22 15:18:49 +0000

    just minor changes

--- lovely_solution/functionalities.py
+++ lovely_solution/functionalities.py
@@ -1,7 +1,7 @@
 import file_manager as fM
 
 def say_hi():
-	print("Hello there! It's lovely to welcome such a loyal customer back! How can I help you?")
+	print("Hello there! It's lovely to welcome such a loyal customer back! How can I help you? test")
 def new_client():
     pass
     
